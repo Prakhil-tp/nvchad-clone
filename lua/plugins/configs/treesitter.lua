72205996a32e06fe4742bcbde5b0ac4a63295098
prakhil <prakhil.premkumar@locationsolutions.com>
2024-02-07 14:55:33 +0400

    Initial commit

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,15 @@
+local options = {
+  ensure_installed = { "lua", "html", "css", "javascript", "typescript", "json" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+  autotag = {
+    enable = true,
+  },
+}
+
+return options
